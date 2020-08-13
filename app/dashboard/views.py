7b1be7a82a29730d30ef476da504dcbdaba735ce
Sebastian <gutierrezgsebastian@outlook.com>
2020-08-13 10:17:39 -0500

    Creando vista para desarrollar el proyecto

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+# TODO aprender vistas basadas en clases
+from django.views import View
+
 # Create your views here.
+def home_view(request):
+    """Vista home
+
+    Args:
+        request (request): Recibe un request
+
+    Returns:
+        render: Devuelve un método render con el html y contexto
+    """
+    context = {}
+    return render(request, "dashboard/index.html", context)
