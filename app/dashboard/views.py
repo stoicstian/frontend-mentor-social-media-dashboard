e3800c135a48394e16ed2b97ab2e78efb9dd6810
Sebastian <gutierrezgsebastian@outlook.com>
2020-08-14 09:24:37 -0500

    Organizando carpetas static y templates para trabajar, agregando fuentes y sass

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -5,13 +5,5 @@ from django.views import View
 
 # Create your views here.
 def home_view(request):
-    """Vista home
-
-    Args:
-        request (request): Recibe un request
-
-    Returns:
-        render: Devuelve un método render con el html y contexto
-    """
     context = {}
-    return render(request, "dashboard/index.html", context)
+    return render(request, "index.html", context)
